2fc30ff2852e916d52b5ab852b5130878a17801d
BatanAndrei <yabatan777@gmail.com>
2024-05-27 14:51:59 +0300

    initial commit

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -7,7 +7,7 @@ import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, 
 const persistConfig = {
     key: 'root',
     storage,
-    whitelist: ['authExtraReducer']
+    whitelist: ['preAuthExtraReducer']
 };
 
 const reducer = combineReducers({
