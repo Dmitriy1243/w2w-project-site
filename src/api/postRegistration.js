f09aed9eaaa13e3aa81cabfde236c60ccdfb2a4d
BatanAndrei <yabatan777@gmail.com>
2024-06-03 00:10:37 +0300

    initial commit

--- src/api/postRegistration.js
+++ src/api/postRegistration.js
@@ -3,9 +3,10 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 export const PostRegistration = createAsyncThunk(
     "auth/postAuth", 
     async ({phone, email, password}, thunkApi) => { 
-//https://dipdeepcode.ru/api
-        const response = await fetch(`https://dipdeepcode.ru/api`, {//http://212.109.223.138:8090/api/auth/register
-            method: 'GET',
+//https://dipdeepcode.ru/api/auth/create
+//https://212.109.223.138:8090/api/auth/register
+        const response = await fetch(`https:///dipdeepcode.ru/api/auth/create`, {
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json',
@@ -14,11 +15,11 @@ export const PostRegistration = createAsyncThunk(
                 'Access-Control-Allow-Headers': 'origin, x-requested-with, content-type',
                 'Access-Control-Allow-Methods' :'PUT, GET, POST, DELETE, OPTIONS'
             },
-        /*    body: JSON.stringify({
+        body: JSON.stringify({
                 phone: phone,
                 email: email,
                 password: password
-                }) */
+                })
         });
 
         const data = await response.json();
@@ -26,7 +27,7 @@ export const PostRegistration = createAsyncThunk(
         if (response.status !== 200) {
         
             return thunkApi.rejectWithValue({
-                message: "Faild request" //message: "Неправильный " 
+                message: "Faild request"
             });
         }
     
